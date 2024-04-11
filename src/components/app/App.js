26fa1cdd448ecd1e57837d0948b5c4d167aa49a3
Kiril Tsonev <kirill.tsonev.work@gmail.com>
2024-04-11 05:08:46 +0300

    fix: updated webpack to fix deployment

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,18 @@
-import './App.sass';
+import "./App.sass";
 import HeaderTop from "../headerTop/HeaderTop";
-import HeaderMiddle from '../headerMiddle/HeaderMiddle';
-import HeaderBottom from '../headerBottom/HeaderBottom';
-import BigPic from '../bigPic/BigPic';
-import Grid from '../grid/Grid';
-import VideoSection from '../videoSection/VideoSection';
-import FiveSections from '../fiveSections/FiveSections';
-import FooterTop from '../footerTop/FooterTop';
-import FooterMiddle from '../footerMiddle/FooterMiddle';
-import FooterBottom from '../footerBottom/FooterBottom';
+import HeaderMiddle from "../headerMiddle/HeaderMiddle";
+import HeaderBottom from "../headerBottom/HeaderBottom";
+import BigPic from "../bigPic/BigPic";
+import Grid from "../grid/Grid";
+import VideoSection from "../videoSection/VideoSection";
+import FiveSections from "../fiveSections/FiveSections";
+import FooterTop from "../footerTop/FooterTop";
+import FooterMiddle from "../footerMiddle/FooterMiddle";
+import FooterBottom from "../footerBottom/FooterBottom";
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <HeaderTop></HeaderTop>
       <HeaderMiddle></HeaderMiddle>
       <HeaderBottom></HeaderBottom>
